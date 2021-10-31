db18392c2ed6b749363714fae3730d3c903e360d
SebastianCasado <therealsebasftw@gmail.com>
2021-10-31 03:45:55 -0400

    retyping code

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // created a variable named 'tabs' and 'document.querrySelectorAll' to select all the id's from the html file //
 const tabs = document.querySelectorAll ('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]') // const that has all the information of my tabs//
 
 // added the command 'tabs.forEach' becuase I want the tabs to loop, I added the action to click in the 'add.Event.Listener' to only show that paricular tab when clicked"
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
-      const target =document.querySelector(tab.dataset.tabTarget)
-      target.classiList.add('active')
+      const target = document.querySelector(tab.dataset.tabTarget)
+      tabContents.forEach(tabContent => {
+        tabContent.classList.remove('active')
+      })
+      tabs.forEach(tab => {
+        tab.classList.remove('active')
+      })
+      tab.classList.add('active')
+      target.classList.add('active')
     })
 })
