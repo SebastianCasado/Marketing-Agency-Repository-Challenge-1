e855f56ce297a5b69f6b9b4257661274bc69a0fe
SebastianCasado <therealsebasftw@gmail.com>
2021-10-31 04:02:23 -0400

    labeled all the importnat changes through comments

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const tabContents = document.querySelectorAll('[data-tab-content]') // const tha
 // added the command 'tabs.forEach' becuase I want the tabs to loop, I added the action to click in the 'add.Event.Listener' to only show that paricular tab when clicked"
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
-      const target = document.querySelector(tab.dataset.tabTarget)
+      const target = document.querySelector(tab.dataset.tabTarget) //getting content for what tab the browser has clicken on//
       tabContents.forEach(tabContent => {
-        tabContent.classList.remove('active')
-      })
+        tabContent.classList.remove('active') //making all the tabs dissapear//
+      }) 
       tabs.forEach(tab => {
-        tab.classList.remove('active')
+        tab.classList.remove('active') //make only the tab they clicked on active//
       })
       tab.classList.add('active')
       target.classList.add('active')
